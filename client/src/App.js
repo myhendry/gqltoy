182c2c467bd2c9a34b96eddfc7e2e5127df0b7b2
Hendry Lim <myhendry@gmail.com>
2018-05-06 21:44:17 +0800

    Updated public file

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,16 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
           <h2>hey there!</h2>
+          <p>
+            Run queries at <a href="/graphiql">/graphiql</a>.
+          </p>
+
+          <p>
+            Read about this{" "}
+            <a href="https://github.com/apollographql/ticketmaster-rest-api-wrapper">
+              server on GitHub
+            </a>.
+          </p>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- src/index.js
+++ src/index.js
@@ -78,21 +78,20 @@ app.get(
   })
 );
 
-// KIV
-// if (process.env.NODE_ENV === "production") {
-//   // Express will serve up production assets
-//   // like our main.js file, or main.css file!
-//   app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
 
-//   // Express will serve up the index.html file
-//   // if it doesn't recognize the route
-//   const path = require("path");
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 const engine = new ApolloEngine({
   apiKey: process.env.ENGINE_API_KEY,
